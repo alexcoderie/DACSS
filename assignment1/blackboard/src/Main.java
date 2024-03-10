7b42faf18cb6da1a58d4eb342ba0aea224af4105
Alexandru Coderie <alexcoderie5@gmail.com>
2024-03-10 15:21:09 +0200

    First instance of the Blackboard style

--- assignment1/blackboard/src/Main.java
+++ assignment1/blackboard/src/Main.java
@@ -1,5 +1,19 @@
+import blackboard.ChairBlackboard;
+import control.Control;
+import knowledge_source.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ChairBlackboard chairBlackboard = new ChairBlackboard();
+        Control control = new Control();
+
+        control.addKnowledgeSource(new CutSeat());
+        control.addKnowledgeSource(new AssembleFeet());
+        control.addKnowledgeSource(new AssembleFeet());
+        control.addKnowledgeSource(new AssembleBackrest());
+        control.addKnowledgeSource(new AssembleStabilizerBar());
+        control.addKnowledgeSource(new PackageChair());
+
+        control.activateKnowledgeSource(chairBlackboard);
     }
 }
---
+++ assignment1/blackboard/src/control/Control.java
@@ -0,0 +1,26 @@
+package control;
+
+import blackboard.ChairBlackboard;
+import knowledge_source.KnowledgeSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Control {
+    private List<KnowledgeSource> knowledgeSourceList;
+
+    public Control() {
+        knowledgeSourceList = new ArrayList<>();
+    }
+
+    public void addKnowledgeSource(KnowledgeSource ks) {
+        knowledgeSourceList.add(ks);
+    }
+
+    public void activateKnowledgeSource(ChairBlackboard chairBlackboard) {
+        for(KnowledgeSource ks : knowledgeSourceList) {
+            if(ks.execCondition(chairBlackboard))
+                ks.execAction(chairBlackboard);
+        }
+    }
+}
